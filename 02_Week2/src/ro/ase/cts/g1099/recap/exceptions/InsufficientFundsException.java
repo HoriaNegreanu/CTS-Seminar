677d2fec25b86b1fa977f58d9835d0f7dc1681b8
HoriaNegreanu <91976484+HoriaNegreanu@users.noreply.github.com>
2022-03-02 08:00:43 +0200

    initialStage - accounts

---
+++ 02_Week2/src/ro/ase/cts/g1099/recap/exceptions/InsufficientFundsException.java
@@ -0,0 +1,11 @@
+package ro.ase.cts.g1099.recap.exceptions;
+
+public class InsufficientFundsException extends Exception {
+	public InsufficientFundsException() {
+		
+	}
+	
+	public InsufficientFundsException(String msg) {
+		super(msg);
+	}
+}
---
+++ 02_Week2/src/ro/ase/cts/g1099/recap/interfaces/Profitable.java
@@ -0,0 +1,6 @@
+package ro.ase.cts.g1099.recap.interfaces;
+
+public interface Profitable {
+	//10%
+	public abstract void addInterest(float interestPercentage);
+}
